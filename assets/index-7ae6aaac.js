09a27c37eedd30526fe80f50e0f2b291617b08b4
Gary <garylin0969@gmail.com>
2023-03-12 21:02:40 +0800

    Updates

---
+++ assets/index-7ae6aaac.js
@@ -0,0 +1,45 @@
+import{s as i,j as e,F as n,a as t,H as r,L as o,I as s}from"./index-9aad05cd.js";const d=i.div`
+    width: 100%;
+    height: 70%;
+    padding: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    p {
+        font-size: 15px;
+        text-indent: 2em;
+        letter-spacing: 2px;
+        line-height: 2;
+        @media (min-width: 576px) {
+            font-size: 20px;
+        }
+    }
+    @media (min-width: 576px) {
+        height: 50%;
+    }
+`,a=i.div`
+    width: 100%;
+    height: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    a {
+        font-size: 30px;
+        text-decoration: none;
+        color: inherit;
+        transition: color 1s, text-decoration 1s ease;
+        &:hover {
+            color: gray;
+            text-decoration: underline;
+        }
+        @media (min-width: 576px) {
+            font-size: 40px;
+        }
+    }
+    @media (min-width: 576px) {
+        height: 50%;
+    }
+`,h=i(s)`
+    height: 50px;
+    margin-right: 15px;
+`,m=()=>e(n,{children:t(r,{children:[e(d,{children:e("p",{children:"成員皆身穿繡著紅雲的黑色風衣，頭戴繫著風鈴的斗笠，相應手指佩戴標有自己代號的戒指（右手大拇指到左手大拇指依次為：零、青、白、朱、玄、空、南、北、三、玉），指甲塗有指甲油，護額上有一道劃痕。平時執行任務時兩人一組一起行動。"})}),t(a,{children:[e(h,{children:e("img",{src:"/images/arrow.svg",alt:"ArrowToMembers"})}),e(o,{to:"/members",children:"點我查看成員資訊"})]})]})});export{m as default};
