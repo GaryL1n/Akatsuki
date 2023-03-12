e8c6969635666f88bdb6d6b81ef23db34bc9e6a2
Gary <garylin0969@gmail.com>
2023-03-12 22:46:31 +0800

    Updates

--- assets/index-44903f89.js
+++ assets/index-44903f89.js
@@ -1,4 +1,4 @@
-import{s as i,j as e,F as n,a as t,H as r,L as o,I as s}from"./index-9aad05cd.js";const d=i.div`
+import{s as i,j as e,F as n,a as t,H as r,L as o,I as s}from"./index-3339f55b.js";const d=i.div`
     width: 100%;
     height: 70%;
     padding: 50px;
@@ -42,4 +42,4 @@ import{s as i,j as e,F as n,a as t,H as r,L as o,I as s}from"./index-9aad05cd.js
 `,h=i(s)`
     height: 50px;
     margin-right: 15px;
-`,m=()=>e(n,{children:t(r,{children:[e(d,{children:e("p",{children:"成員皆身穿繡著紅雲的黑色風衣，頭戴繫著風鈴的斗笠，相應手指佩戴標有自己代號的戒指（右手大拇指到左手大拇指依次為：零、青、白、朱、玄、空、南、北、三、玉），指甲塗有指甲油，護額上有一道劃痕。平時執行任務時兩人一組一起行動。"})}),t(a,{children:[e(h,{children:e("img",{src:"/images/arrow.svg",alt:"ArrowToMembers"})}),e(o,{to:"/members",children:"點我查看成員資訊"})]})]})});export{m as default};
+`,m=()=>e(n,{children:t(r,{children:[e(d,{children:e("p",{children:"成員皆身穿繡著紅雲的黑色風衣，頭戴繫著風鈴的斗笠，相應手指佩戴標有自己代號的戒指（右手大拇指到左手大拇指依次為：零、青、白、朱、玄、空、南、北、三、玉），指甲塗有指甲油，護額上有一道劃痕。平時執行任務時兩人一組一起行動。"})}),t(a,{children:[e(h,{children:e("img",{src:"./images/arrow.svg",alt:"ArrowToMembers"})}),e(o,{to:"/members",children:"點我查看成員資訊"})]})]})});export{m as default};
