dc5c7b22ff09024b52284805913883ed92dfe6b8
Gary <garylin0969@gmail.com>
2023-03-13 01:40:50 +0800

    改用HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Loading from './components/Loading';
 import NotFound from './components/NotFound';
@@ -12,13 +12,13 @@ function App() {
     return (
         <>
             <GlobalStyle />
-            <Router basename='/akatsuki'>
+            <Router>
                 <Suspense fallback={<Loading />}>
                     <Layout>
                         <Routes>
                             <Route path="/" element={<Home />} />
                             <Route path="/members" element={<Members />} />
-                            <Route path="/*" element={<NotFound />} />
+                            <Route path="*" element={<NotFound />} />
                         </Routes>
                     </Layout>
                 </Suspense>
